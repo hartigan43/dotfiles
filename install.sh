fff2e056a0143055308ca087feca614d48d6722d
Jake Hartigan <hartigan43@gmail.com>
2013-03-01 10:06:13 -0500

    A bit more restructing and furthered installation process

--- install.sh
+++ install.sh
@@ -1,23 +1,20 @@
 #! /bin/bash
 #Detect uname for proper os configuration
 unamestr=$( uname -s )
-linux=''
 
-#TODO ADD UBUNTU CONFIG AS WELL AS ARCH WHEN WORKING
-#TODO .vimrcs folder for diff configs
 #uname case function
 getOS() {
   case $unamestr in
     Darwin) os=0;;
-     Linux) os=1; if [[ -n $linux ]]; then linuxVersion(); fi;;
+     Linux) os=1; if [ -n $linux ]; then linuxVersion; fi;;
          *) echo -e "Uname was nor properly detected, this script will not complete properly.\n";;
   esac
 }
 
-#choose between my two main linux instalations Arch for desktop and ubuntu server
+#choose between my two main linux distributions
 linuxVersion() {
   while true; do
-    read -p "Enter a 1 if using Arch, enter 2 if using Ubuntu" lin
+    read -p "Enter a 1 if using Arch, enter 2 if using Ubuntu: " lin
     case $lin in
       1) linux=1; break;;
       2) linux=2; break;;
@@ -25,6 +22,9 @@ linuxVersion() {
     esac
   done
 }
+
+echo -e "$unamestr detected!\n"
+getOS
 
 #Install zsh and git
 echo -e "Installing zsh and git...\n"
@@ -40,6 +40,8 @@ fi
 #Install oh-my-zsh
 echo -e "Installing oh-my-zsh...\n"
 curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+#in case prompt fails for zsh
+chsh -s /bin/zsh
 
 #Install vimrc zshrc and functions
 echo -e "Copying vimrc, zshrc, tmux.conf and such to HOME...\n"
@@ -60,12 +62,35 @@ else
   cp .zsh/.zshrc_linux "$HOME/.zshrc"
 fi
 
+echo -e "Running basic git configuration...\n"
 read -p "Enter your name (full name): " name
 read -p "Enter your git email address: " email
 git config --global user.name "$name"
 git config --global user.email "$email"
 
 echo -e "Installing Vundle and running BundleInstall for vim plugins...\n"
-echo -e "NOTE: YOU WILL HAVE TO BUILD THE YCM AND POWERLINE DEPENDENCIES FOR YOUR MACHINE!:"
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 vim +BundleInstall +qall
+
+#YCM BUILD AND POWERLINE INSTALLATION - CURRENTLY NOT AUTOMATED IN OSX DUE TO GOOFY PYTHON ISSUS WITH HOMEBREW / SYSTEM
+if [[ $os -ne 0 ]]; then
+  if [[ $linux -eq 2]]; then  #UBUNTU 
+    sudo apt-get install build-essential cmake python-dev
+    cd ~/.vim/bundle/YouCompleteme
+    ./install.sh --clang-completer
+    sudo apt-get install python-pip
+    pip install --user git+git://github.com/Lokaltog/powerline
+    echo "set rtp+=$HOME/.local/lib/python2.7/site-packages/powerline/bindings/vim" >> $HOME/.vimrc
+    mkdir $HOME/.fonts && cd $HOME/.fonts
+    wget https://github.com/Lokaltog/powerline-fonts/tree/master/Inconsolata/Inconsolata\ for\ Powerline.otf
+    fc-cache -vf $HOME/.fonts
+  else   #ARCH
+    sudo pacman -S clang cmake
+    mkdir ~/ycm_build && cd ~/ycm_build
+    cmake -G "Unix Makefiles" -DUSE_SYSTEM_LIBCLANG=ON . ~/.vim/bundle/YouCompleteMe/cpp
+    make ycm_core
+    cp /usr/lib/llvm/libclang.so ~/.vim/bundle/YouCompleteMe/python
+    ehco -e "YCM complete, installing powerline...\n"
+    packer -S python2-powerline-git
+  fi
+fi
