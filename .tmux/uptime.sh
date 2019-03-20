a5eec1160055a09b99b3115c56cadde168eb8128
Jake Hartigan <hartigan43@gmail.com>
2019-03-20 11:36:51 -0400

    more reliable uptime display

--- .tmux/uptime.sh
+++ .tmux/uptime.sh
@@ -1,2 +1,3 @@
 #! /bin/bash
-uptime | awk -F'( |,|:)+' '{print $6"d "$8"h "$9"m"}'
+# from https://unix.stackexchange.com/questions/83404/how-do-i-show-simple-uptime-in-tmuxs-status-bar
+awk '{printf("%01dd %01dh %02dm",($1/60/60/24),($1/60/60%24),($1/60%60))}' /proc/uptime
