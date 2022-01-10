f9978f4f1259234ed3ee5e6f0de5e54381bf05ae
Jake Hartigan <hartigan@rowan.edu>
2022-01-10 15:41:21 -0500

    battery check fixed to properly update if device has a battery

--- .tmux/battery.sh
+++ .tmux/battery.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 # check if the device has a battery and show state / percentage if so
 
-batteryCheck = $(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep percentage)
-if [ -z "${battherChecky##*ignored*}" ]; then
-$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep state | tr -s " " | cut -d ':' -f 2)$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep percentage | tr -s " " | cut -d ":" -f 2)
-else
+batteryCheck=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep percentage)
+if [ -z "${batteryCheck##*ignored*}" ]; then
   echo " "
+else
+  echo $(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep state | tr -s " " | cut -d ':' -f 2)$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep percentage | tr -s " " | cut -d ":" -f 2)
 fi
