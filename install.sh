2c9c771585ff70af2009623543c45374e3e737cc
Jake Hartigan <jake.hartigan@umbra.space>
2023-11-02 13:27:35 -0400

    cleaned up and fixed variable s $VAR -> ${VAR}

--- install.sh
+++ install.sh
@@ -1,21 +1,20 @@
 #!/usr/bin/env sh
-# TODO Variables from $varto "${var}"
 
 # do not allow run as root - thanks @freekingdean
-if [ "$EUID" -eq 0 ]; then
+if [ "${EUID}" -eq 0 ]; then
   echo "This script should not be run as root!"
   exit 1
 fi
 
 # make workspace and misc
-mkdir -p "$HOME/Workspace/misc"
+mkdir -p "${HOME}/Workspace/misc"
 
 #### PLATFORM AND PACKAGES ####
 
-PLATFORM=$(uname)
-if [ "$PLATFORM" = "linux" ] || [ "$PLATFORM" = "Linux" ]; then
-  PLATFORM=$(cat /etc/*-release | grep ^ID= | sed 's/^ID=\(.*\)$/\1/')
-  if [ "$PLATFORM" = "" ]; then
+PLATFORM="$(uname)"
+if [ "${PLATFORM}" = "linux" ] || [ "${PLATFORM}" = "Linux" ]; then
+  PLATFORM="$(cat /etc/*-release | grep ^ID= | sed 's/^ID=\(.*\)$/\1/')"
+  if [ "${PLATFORM}" = "" ]; then
     if [ -x "$(command -v pacman)" ]; then
       PLATFORM="arch"
     fi
@@ -23,42 +22,42 @@ if [ "$PLATFORM" = "linux" ] || [ "$PLATFORM" = "Linux" ]; then
 fi
 
 PACKAGES="zsh ruby git curl wget neovim tmux openssh go docker tree"
-EDITOR="vim"
+EDITOR="${EDITOR:-$(command -v vim)}"
 
-if [ "$PLATFORM" = "centos" ]; then
+if [ "${PLATFORM}" = "centos" ]; then
   PACKAGER="sudo yum -y"
   PACKAGER_INSTALL="install"
   PACKAGER_UPDATE="update"
   PACKAGER_UPGRADE="upgrade"
-  PACKAGES="$PACKAGES python3 python3-pip openssl-devel readline-devel zlib-devel"
-elif [ "$PLATFORM" = "debian" ]; then
+  PACKAGES="${PACKAGES} python3 python3-pip openssl-devel readline-devel zlib-devel"
+elif [ "${PLATFORM}" = "debian" ]; then
   PACKAGER="sudo apt-get -y"
   PACKAGER_INSTALL="install"
   PACKAGER_UPDATE="update"
   PACKAGER_UPGRADE="upgrade"
-  PACKAGES="$PACKAGES python3 python3-pip libssl-dev libreadline-dev zlib1g-dev"
+  PACKAGES="${PACKAGES} python3 python3-pip libssl-dev libreadline-dev zlib1g-dev"
   sudo cp /usr/bin/pip3 /usr/bin/pip
-elif [ "$PLATFORM" = "fedora" ]; then
+elif [ "${PLATFORM}" = "fedora" ]; then
   PACKAGER="sudo dnf -y"
   PACKAGER_INSTALL="install"
   PACKAGER_UPDATE="update"
   PACKAGER_UPGRADE="upgrade"
-  PACKAGES="$PACKAGES python3 python3-pip openssl-devel readline-devel zlib-devel"
-elif [[ "$PLATFORM" = "arch" || "$PLATFORM" = "archarm" ]]; then
+  PACKAGES="${PACKAGES} python3 python3-pip openssl-devel readline-devel zlib-devel"
+elif [[ "${PLATFORM}" = "arch" || "${PLATFORM}" = "archarm" ]]; then
   PACKAGER="sudo pacman --noconfirm"
   PACKAGER_INSTALL="-S"
   PACKAGER_UPDATE="-Syu"
   PACKAGER_UPGRADE="-Syu"
-  PACKAGES="$PACKAGES base-devel python python-pip cmake vim neovim python-pynvim"
-elif [ "$PLATFORM" = "Darwin" ]; then
+  PACKAGES="${PACKAGES} base-devel python python-pip cmake vim neovim python-pynvim"
+elif [ "${PLATFORM}" = "Darwin" ]; then
   # /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
   PACKAGER="brew"
   PACKAGER_INSTALL="install"
   PACKAGER_UPDATE="update"
   PACKAGER_UPGRADE="upgrade"
-  PACKAGES="$PACKAGES python telnet watch htop glances kubectx infracost pygments ccat"
+  PACKAGES="${PACKAGES} python telnet watch htop glances kubectx infracost pygments ccat"
 else
-  echo "platform '$PLATFORM' is not supported"
+  echo "platform '${PLATFORM}' is not supported"
   exit 1
 fi
 
@@ -68,7 +67,7 @@ fi
 confirm() {
   echo "Do you want to $1? [y/N] "
   read response
-  response=$(echo $response  | tr '[:upper:]' '[:lower:]')
+  response="$(echo $response  | tr '[:upper:]' '[:lower:]')"
 
   if [[ "$response" =~ ^(yes|y)$ ]]; then
     return 0
@@ -77,25 +76,27 @@ confirm() {
   fi
 }
 
-
 # configure the global get settings for name, email, and editor
 gitConfig() {
   echo -e "Running basic git configuration...\n"
-  echo -e "Enter your name (full name) for git: "; read name
+  echo -e "Enter your full name for git: "; read name
   echo -e "Enter your git email address: "; read email
-  git config --global user.name "$name"
-  git config --global user.email "$email"
-  git config --global core.editor $EDITOR
+  git config --global user.name "${name}"
+  git config --global user.email "${email}"
+  git config --global core.editor "${EDITOR}"
   git config --global init.defaultBranch "main"
+  if confirm "add global git aliases"; then
+    git config --global alias.main '!git fetch && git checkout main && git pull origin main'
+  fi
 }
 
 # clone and install patched nerdfonts to work with airline and powerline symbols locally
 installNERDFonts() {
-  echo -e "Installing Powerline compatible Nerd Fonts..."
-  mkdir -p "$HOME/.local/share/fonts"
+  echo -e "Installing Nerd Fonts..."
+  mkdir -p "${HOME}/.local/share/fonts"
 
-  git clone --depth 1 https://github.com/ryanoasis/nerd-fonts.git "$HOME/Workspace/misc/nerd-fonts"
-  cd "$HOME/Workspace/misc/nerd-fonts" || return
+  git clone --depth 1 https://github.com/ryanoasis/nerd-fonts.git "${HOME}/Workspace/misc/nerd-fonts"
+  cd "${HOME}/Workspace/misc/nerd-fonts" || return
   ./install.sh Inconsolata
   ./install.sh InconsolataGo
   ./install.sh Iosevka
@@ -104,27 +105,27 @@ installNERDFonts() {
 
 # install fzf
 fzfInstall() {
-  if [ ! -d "$HOME/.fzf" ]; then
-    git clone --depth 1 https://github.com/junegunn/fzf.git "$HOME/.fzf"
-    cd "$HOME/.fzf" && ./install
+  if [ ! -d "${HOME}/.fzf" ]; then
+    git clone --depth 1 https://github.com/junegunn/fzf.git "${HOME}/.fzf"
+    cd "${HOME}/.fzf" && ./install
   fi
 }
 
 # Set real username
 setRealName() {
   echo -e "Please enter you real name, for your user account. ex: John Doe:\n"; read realName
-  currentUser=$(whoami)
-  sudo usermod -c "'$realName'" $currentUser
+  currentUser="$(whoami)"
+  sudo usermod -c "'${realName}' ${currentUser}"
 }
 
 installBasics() {
-  mkdir -p "$HOME/.local/bin"
+  mkdir -p "${HOME}/.local/bin"
 
   # first update and install of packages
-  echo -e "Updating then installing zsh wget curl git tmux...\n"
-  $PACKAGER $PACKAGER_UPDATE
-  $PACKAGER $PACKAGER_UPGRADE
-  $PACKAGER $PACKAGER_INSTALL "${PACKAGES}"
+  echo -e "Updating then installing ${PACKAGES}...\n"
+  "${PACKAGER}" "${PACKAGER_UPDATE}"
+  "${PACKAGER}" "${PACKAGER_UPGRADE}"
+  "${PACKAGER}" "${PACKAGER_INSTALL}" "${PACKAGES}"
 
   # install zcomet
   echo -e "Cloning zcomet into ~/.zcomet ...\n"
@@ -133,34 +134,34 @@ installBasics() {
   # Symlink vimrc, zshrc and aliases/functions
   echo -e "Symlinks for vimrc, zshrc, tmux.conf, etc to HOME...\n"
   #backup any original config files
-  mv "$HOME/.zshrc" "$HOME/.zshrc.bak"
-  mv "$HOME/.bashrc" "$HOME/.bashrc.bak"
-  mv "$HOME/.bash_profile" "$HOME/.bash_profile.bak"
+  mv "${HOME}/.zshrc" "${HOME}/.zshrc.bak"
+  mv "${HOME}/.bashrc" "${HOME}/.bashrc.bak"
+  mv "${HOME}/.bash_profile" "${HOME}/.bash_profile.bak"
 
   # vim/nvim, config directories
-  mkdir -p "$HOME/.config/vim"
-  mkdir -p "$HOME/.vim/colors"
-  mkdir -p "$HOME/.config/nvim/colors"
-  mkdir -p "$HOME/.config/alacritty/themes"
+  mkdir -p "${HOME}/.config/vim"
+  mkdir -p "${HOME}/.vim/colors"
+  mkdir -p "${HOME}/.config/nvim/colors"
+  mkdir -p "${HOME}/.config/alacritty/themes"
 
   # symlinks to $HOME
-  ln -s "$HOME/.dotfiles/bash/.bash_profile" "$HOME/.bash_profile"
-  ln -s "$HOME/.dotfiles/bash/.bashrc" "$HOME/.bashrc"
-  ln -s "$HOME/.dotfiles/alacritty/alacritty.yml" "$HOME/.config/alacritty/alacritty.yml"
-  ln -s "$HOME/.dotfiles/misc/.common.sh" "$HOME/.common.sh"
-  ln -s "$HOME/.dotfiles/misc/.editorconfig" "$HOME/.editorconfig"
-  ln -s "$HOME/.dotfiles/tmux/.tmux.conf" "$HOME/.tmux.conf"
-  ln -s "$HOME/.dotfiles/tmux" "$HOME/.tmux"
-  ln -s "$HOME/.dotfiles/vim/.vimrc" "$HOME/.vimrc"
-  ln -s "$HOME/.dotfiles/vim/.vimrc" "$HOME/.config/nvim/init.vim"
-  ln -s "$HOME/.dotfiles/zsh/.zshrc" "$HOME/.zshrc"
+  ln -s "${HOME}/.dotfiles/bash/.bash_profile" "${HOME}/.bash_profile"
+  ln -s "${HOME}/.dotfiles/bash/.bashrc" "${HOME}/.bashrc"
+  ln -s "${HOME}/.dotfiles/alacritty/alacritty.yml" "${HOME}/.config/alacritty/alacritty.yml"
+  ln -s "${HOME}/.dotfiles/misc/.common.sh" "${HOME}/.common.sh"
+  ln -s "${HOME}/.dotfiles/misc/.editorconfig" "${HOME}/.editorconfig"
+  ln -s "${HOME}/.dotfiles/tmux/.tmux.conf" "${HOME}/.tmux.conf"
+  ln -s "${HOME}/.dotfiles/tmux" "${HOME}/.tmux"
+  ln -s "${HOME}/.dotfiles/vim/.vimrc" "${HOME}/.vimrc"
+  ln -s "${HOME}/.dotfiles/vim/.vimrc" "${HOME}/.config/nvim/init.vim"
+  ln -s "${HOME}/.dotfiles/zsh/.zshrc" "${HOME}/.zshrc"
 
   # use find and cp -s to add symlink for any .vim colors files
-  find "$HOME/.dotfiles/vim/colors/" -type f -name "*.vim" -exec cp -s {} "$HOME/.vim/colors/" \;
-  find "$HOME/.dotfiles/vim/colors/" -type f -name "*.vim" -exec cp -s {} "$HOME/.config/nvim/colors/" \;
+  find "${HOME}/.dotfiles/vim/colors/" -type f -name "*.vim" -exec cp -s {} "${HOME}/.vim/colors/" \;
+  find "${HOME}/.dotfiles/vim/colors/" -type f -name "*.vim" -exec cp -s {} "${HOME}/.config/nvim/colors/" \;
   # do the same for alacritty themes after cloning public themes repo
-  git clone https://github.com/alacritty/alacritty-theme $HOME/.config/alacritty/themes
-  find "$HOME/.dotfiles/alacritty/themes" -type f -name "*.yml" -exec cp -s {} "$HOME/.config/alacritty/themes/themes/" \;
+  git clone https://github.com/alacritty/alacritty-theme ${HOME}/.config/alacritty/themes
+  find "${HOME}/.dotfiles/alacritty/themes" -type f -name "*.yml" -exec cp -s {} "${HOME}/.config/alacritty/themes/themes/" \;
 }
 
 #### Run it ####
