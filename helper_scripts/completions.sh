903065466e53cb8d375a5f7059b981927904b466
Jake Hartigan <hartiganj@protonmail.com>
2024-08-29 14:08:09 -0400

    update completions script to pull from mise, can manually add others if needed

--- helper_scripts/completions.sh
+++ helper_scripts/completions.sh
@@ -7,21 +7,24 @@ shell=$(basename "$SHELL")
 completion_dir="${HOME}/.config/${shell}/completions"
 
 install_completions() {
-  local tools=(
-    "kubectl"
-    "mise"
-    "spacectl"
-  )
+  #create an array of tools installed via mise
+  local tools=()
+  while IFS="" read -r line ; do tools+=("$line") ; done < <(mise list | awk 'NR>0 {print $1}' | cut -d: -f1)
+  # sc doesn't like the line below - https://www.shellcheck.net/wiki/SC2207
+  # local tools=($(mise list | awk 'NR>1 {print $1}' | cut -d: -f1))
 
   # check for completion directory and create if not
   [[ -d "${completion_dir}" ]] || mkdir -p "${completion_dir}"
 
   for tool in "${tools[@]}"; do
     if command_exists "${tool}" ; then
-      "${tool}" completion "${shell}" > "${completion_dir}/_${tool}"
+      if "${tool}" completion &> /dev/null; then
+        echo "Installing ${tool} completions..."
+        "${tool}" completion "${shell}" > "${completion_dir}/_${tool}"
+      fi
     fi
   done
 
   echo "completions installed.  Make sure to add them to your fpath.  ex: fpath=(~/.config/zsh/completions \$fpath)"
 }
-
+# vim: set ft=sh et:
