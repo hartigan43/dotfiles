ce92e488ce005f86fd728ec29e8ba2b4670b1000
Jake Hartigan <jake.hartigan@umbra.space>
2025-06-20 12:35:52 -0400

    tmux: cross platform battery and uptime changes

--- tmux/battery.sh
+++ tmux/battery.sh
@@ -1,9 +1,50 @@
-#! /bin/bash
-# check if the device has a battery and show state / percentage if so
+#!/bin/bash
+# Cross-platform battery status script with optional Nerd Font icons
 
-batteryCheck=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep percentage)
-if [ -z "${batteryCheck##*ignored*}" ]; then
-  echo " "
+USE_ICON_FONT=true  # Set to false if you don't have Nerd Fonts
+
+declare -A state_map_icon
+state_map_icon[discharging]="󱟤"
+state_map_icon[charging]="󰂄"
+state_map_icon[charged]="󱟢"
+
+declare -A state_map_label
+state_map_label[discharging]="DIS"
+state_map_label[charging]="CHR"
+state_map_label[charged]="FULL"
+
+get_state_display() {
+  local state="$1"
+  if [ "$USE_ICON_FONT" = true ]; then
+    echo "${state_map_icon[$state]:-${state_map_label[$state]}}"
+  else
+    echo "${state_map_label[$state]}"
+  fi
+}
+
+# Prefer TMUX_OS_TYPE if set, otherwise fall back to uname
+OS_TYPE="${TMUX_OS_TYPE:-$(uname | tr '[:upper:]' '[:lower:]')}"
+
+if [[ "$OS_TYPE" == "darwin" ]]; then
+  # macOS
+  battery_info=$(pmset -g batt)
+  percentage=$(echo "$battery_info" | grep -o '[0-9]\{1,3\}%')
+  state=$(echo "$battery_info" | grep -o 'discharging\|charging\|charged')
+  if [ -n "$percentage" ] && [ -n "$state" ]; then
+    display=$(get_state_display "$state")
+    echo " $percentage $display "
+  else
+    echo " "
+  fi
 else
-  echo $(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep state | tr -s " " | cut -d ':' -f 2)$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep percentage | tr -s " " | cut -d ":" -f 2)
+  # Assume Linux with upower
+  batteryCheck=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep percentage)
+  if [ -z "${batteryCheck##*ignored*}" ]; then
+    echo " "
+  else
+    state=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep state | tr -s " " | cut -d ':' -f 2 | xargs)
+    percent=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep percentage | tr -s " " | cut -d ":" -f 2 | xargs)
+    display=$(get_state_display "$state")
+    echo " $percent $display "
+  fi
 fi
--- tmux/uptime.sh
+++ tmux/uptime.sh
@@ -1,3 +1,22 @@
-#! /bin/bash
-# from https://unix.stackexchange.com/questions/83404/how-do-i-show-simple-uptime-in-tmuxs-status-bar
-awk '{printf("%01dd %01dh %02dm",($1/60/60/24),($1/60/60%24),($1/60%60))}' /proc/uptime
+#!/bin/bash
+# Cross-platform uptime script for Linux and macOS, using TMUX_OS_TYPE if available
+
+# Prefer TMUX_OS_TYPE if set, otherwise fall back to uname
+OS_TYPE="${TMUX_OS_TYPE:-$(uname | tr '[:upper:]' '[:lower:]')}"
+
+if [[ "$OS_TYPE" == "darwin" ]]; then
+  # macOS
+  boot_time_raw=$(sysctl -n kern.boottime)
+  boot_time=$(echo "$boot_time_raw" | awk -F'[ ,}]+' '{print $4}')
+  now=$(date +%s)
+  uptime_seconds=$((now - boot_time))
+else
+  # Linux
+  uptime_seconds=$(awk '{print int($1)}' /proc/uptime)
+fi
+
+days=$((uptime_seconds/60/60/24))
+hours=$(( (uptime_seconds/60/60)%24 ))
+minutes=$(( (uptime_seconds/60)%60 ))
+
+printf "%01dd %01dh %02dm\n" "$days" "$hours" "$minutes"
